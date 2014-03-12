8058e562eaaa88430e6799dd898d8de2a8f45fc0
daid <daid303@gmail.com>
2014-03-12 15:47:25 +0100

    Make sure the platform level is configured.

--- Marlin/UltiLCD2_menu_first_run.h
+++ Marlin/UltiLCD2_menu_first_run.h
@@ -2,7 +2,7 @@
 #define ULTI_LCD2_MENU_FIRST_RUN_H
 
 #define EEPROM_FIRST_RUN_DONE_OFFSET 0x400
-#define IS_FIRST_RUN_DONE() (eeprom_read_byte((const uint8_t*)EEPROM_FIRST_RUN_DONE_OFFSET) == 'U')
+#define IS_FIRST_RUN_DONE() ((eeprom_read_byte((const uint8_t*)EEPROM_FIRST_RUN_DONE_OFFSET) == 'U') && add_homeing[2] != 0.0)
 #define SET_FIRST_RUN_DONE() do { eeprom_write_byte((uint8_t*)EEPROM_FIRST_RUN_DONE_OFFSET, 'U'); } while(0)
 
 void lcd_menu_first_run_init();
