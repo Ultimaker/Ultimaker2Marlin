9b924c7d6265ad84ef61c08eb3f80f70e5cb1d07
daid <daid303@gmail.com>
2015-01-14 10:05:41 +0100

    Fix X offset of UM2go

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -336,7 +336,7 @@ const bool Z_ENDSTOPS_INVERTING = true; // set to true to invert the logic of th
 #define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
 // Travel limits after homing
 #define X_MAX_POS 122
-#define X_MIN_POS -10
+#define X_MIN_POS -14
 #define Y_MAX_POS 124
 #define Y_MIN_POS 0
 #define Z_MAX_POS 130
