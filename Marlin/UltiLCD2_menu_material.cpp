c5728ad7c82f0585d37f80e8bf087e11fc87aeeb
daid <daid303@gmail.com>
2015-02-13 09:30:30 +0100

    Fix the E position after a material change.

--- Marlin/UltiLCD2_menu_material.cpp
+++ Marlin/UltiLCD2_menu_material.cpp
@@ -297,7 +297,6 @@ static void materialInsertReady()
     plan_set_e_position(0);
     plan_buffer_line(current_position[X_AXIS], current_position[Y_AXIS], current_position[Z_AXIS], -END_OF_PRINT_RETRACTION / volume_to_filament_length[active_extruder], 25*60, active_extruder);
     cancelMaterialInsert();
-
 }
 
 static void lcd_menu_change_material_insert()
--- Marlin/UltiLCD2_menu_print.cpp
+++ Marlin/UltiLCD2_menu_print.cpp
@@ -499,6 +499,7 @@ static void lcd_menu_print_heatup()
 
 static void lcd_change_to_menu_change_material_return()
 {
+    plan_set_e_position(current_position[E_AXIS]);
     setTargetHotend(material[active_extruder].temperature, active_extruder);
     currentMenu = lcd_menu_print_printing;
 }
