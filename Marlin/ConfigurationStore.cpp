f0f9fae1d1f373abe44df60b528e877c88d840eb
armin-fx <rechner_x@yahoo.de>
2016-01-25 03:01:01 +0100

    use EEPROM update function
    EEPROM_write_xxx() writes directly to EEPROM,
    but EEPROM_update_xxx() read the data before and write only when it is changed.

--- Marlin/ConfigurationStore.cpp
+++ Marlin/ConfigurationStore.cpp
@@ -9,7 +9,7 @@ void _EEPROM_writeData(int &pos, uint8_t* value, uint8_t size)
 {
     do
     {
-        eeprom_write_byte((unsigned char*)pos, *value);
+        eeprom_update_byte((unsigned char*)pos, *value);
         pos++;
         value++;
     }while(--size);
