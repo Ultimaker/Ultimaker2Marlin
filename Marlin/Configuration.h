269897eca3354d9bda065da658bfec43896bf68e
daid <daid303@gmail.com>
2016-06-30 10:46:40 +0200

    Fix Y build volume so Y0 is no longer touching the front of the machine.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -337,7 +337,7 @@ const bool Z_ENDSTOPS_INVERTING = true; // set to true to invert the logic of th
 // Travel limits after homing
 #define X_MAX_POS 230
 #define X_MIN_POS 0
-#define Y_MAX_POS 230
+#define Y_MAX_POS 223
 #define Y_MIN_POS 0
 #define Z_MAX_POS 230
 #define Z_MIN_POS 0
