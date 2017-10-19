1eade54a237a2d325a37cf7bfe92814e831f2939
TinkerGnome <tinkergnome@gmx.de>
2017-10-19 21:40:17 +0200

    tag V17.10.1

--- Marlin/package.sh
+++ Marlin/package.sh
@@ -8,7 +8,7 @@
 #############################
 
 ##Which version name are we appending to the final archive
-export BUILD_NAME=17.10
+export BUILD_NAME=17.10.1
 
 #############################
 # Actual build script
